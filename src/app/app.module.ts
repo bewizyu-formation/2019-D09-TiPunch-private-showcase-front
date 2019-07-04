5093351fbbe2f8170bd441ea5c21674dea29b06c
Battistini <battistini.anthony@gmail.com>
2019-07-04 17:33:34 +0200

    Configuration header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {SignupComponent} from './components/signup/signup.component';
 import {environment} from '../environments/environment';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {APP_CONFIG} from './app.config';
 import {TokenInterceptorService} from './services/interceptors/token-interceptor.service';
 import {ErrorInterceptorService} from './services/interceptors/error-interceptor.service';
 import {CommonHeadersInterceptorService} from './services/interceptors/common-headers-interceptor.service';
-<<<<<<< HEAD
 import {MatButtonModule} from '@angular/material/button';
-=======
-import { SignupComponent } from './signup/signup.component';
->>>>>>> 0c3948b102d7655a93adf2d5af49c613cef48422
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -22,7 +20,8 @@ import { SignupComponent } from './signup/signup.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule
   ],
   providers: [
     {provide: APP_CONFIG, useValue: environment},
