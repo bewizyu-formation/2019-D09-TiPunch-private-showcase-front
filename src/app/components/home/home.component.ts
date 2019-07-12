f72b1e3e466292325665dbd1224b1f11183fb7b8
Yengo <jerzy.yengo@gmail.com>
2019-07-12 12:17:26 +0200

    page home test passed

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,16 +12,16 @@ export class HomeComponent implements OnInit {
 
   title = 'Artistes dans votre departement';
 
-  artists: Array<Artist> = [new Artist('The weeknd','deded','tipunch@test','lyon',3, 'Starboy', '../../assets/startboy.jpg'),
-                            new Artist('Kyo','kyo','kyo@test', 'paris' , 12, 'Derniere dance', '../../assets/Dans-La-Peau-Digipack.jpg'),
-                            new Artist('Michael jackson', 'mj','mj@test','usa', 16, 'The king of pop','../../assets/michaeljackson.jpg')
-                          ];
+  artists: Array<Artist> = [new Artist('The weeknd', 'deded', 'tipunch@test', 'lyon', 3, 'Starboy', '../../assets/startboy.jpg'),
+  new Artist('Kyo', 'kyo', 'kyo@test', 'paris', 12, 'Derniere dance', '../../assets/Dans-La-Peau-Digipack.jpg'),
+  new Artist('Michael jackson', 'mj', 'mj@test', 'usa', 16, 'The king of pop', '../../assets/michaeljackson.jpg')
+  ];
 
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
-goToArtist(event: Event){
-  this.router.navigate([PATH_ARTIST, 'TOTO']);
-}
+  goToArtist(event: Event) {
+    this.router.navigate([PATH_ARTIST, 'TOTO']);
+  }
 }
